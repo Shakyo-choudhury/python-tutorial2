c0fe31d96e30416b3fd3faa37556bc2320799455
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-06-06 21:50:48 +0530

    Add files via upload

---
+++ tutorial2.py
@@ -0,0 +1,38 @@
+# this is a comment
+# print("Hello universe")
+# print("Hello everyone")
+# print("Hello campers")
+
+
+"""
+	   This is a multi-line comment with docstrings
+
+	   print("Hello world")
+	   print("Hello universe")
+	   print("Hello everyone")
+       print("hey coders")
+	   
+ to be printed -  hey what's up?
+example: print('hey, what\'s up?')
+
+multiline strings
+# \n - for new line
+print("Multiline strings\ncan be created\nusing escape sequences.")
+
+for printing my file path we can use escape sequence characters
+print("C:\\Users\\SHAKYO\\Desktop\\python programming series")
+
+"""
+
+
+# print(r"Backslashes \ don't need to be escaped in raw strings.")
+
+# error now
+
+# print(r"There's an unescaped backslash at the end of this string\")
+
+
+
+print('\\i am shakyo')
+print('who\'re you?')
+print("shakyo told: \"he is a human. \" ")
